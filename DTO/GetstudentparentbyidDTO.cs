ca5fe4eff27463d3a48b210d9a4f80b7508de1a8
Mohamed-Hatem-Ali <mohmmedhatemali43@std.mans.edu.eg>
2024-10-09 23:52:18 +0300

    update Exam+ParentController

---
+++ DTO/GetstudentparentbyidDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace final_project_Api.DTO
+{
+    public class GetstudentparentbyidDTO
+    {
+        public string StudentId { get; set; }
+        public string StudentName { get; set; }
+    }
+}
